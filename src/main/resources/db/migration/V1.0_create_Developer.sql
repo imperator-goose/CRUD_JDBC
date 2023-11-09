37bea46ae5386f960799ca3dded8f4dfe6ddd54b
TandDA <tanduevdmitriy@gmail.com>
2023-11-09 18:33:11 +0300

    first commit

---
+++ src/main/resources/db/migration/V1.0_create_Developer.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Developer (
+    INT ID NOT NULL;
+    firstname VARCHAR(20) NOT NULL;
+    lastname VARCHAR(20) NOT NULL;
+    skills VARCHAR(50);
+    specialty VARCHAR(30);
+);
